c7733295bc2ad9149c7a1baa902560a7e9cae766
TNK_Thanakrit <phumpuang_t@silpakorn.edu>
2025-01-28 02:02:20 +0700

    Revert "= Thai Language"
    This reverts commit 44734c68d316169cfc7d6faa99277d9919d00d43.

--- Assets/0-Scripts/INameInput.cs
+++ Assets/0-Scripts/INameInput.cs
@@ -13,7 +13,6 @@ public class INameInput : MonoBehaviour
     public TextMeshProUGUI p2DisplayText; // Player 2's name display text
 
     private const int MaxNameLength = 12; // Maximum number of characters allowed for a name
-    private const int MaxThaiNameLength = 12; // Maximum number of Thai characters allowed for a name
 
     private void Start()
     {
@@ -26,16 +25,10 @@ public class INameInput : MonoBehaviour
         string player2Name = INameData.LoadPlayerName(2);
 
         // Update UI with loaded names or default messages
-        p1DisplayText.text = !string.IsNullOrEmpty(player1Name) ? player1Name : "Player 1";
-        p2DisplayText.text = !string.IsNullOrEmpty(player2Name) ? player2Name : "Player 2";
 
         // Add listeners for end edit events
         p1InputField.onEndEdit.AddListener(OnPlayer1NameSubmitted);
         p2InputField.onEndEdit.AddListener(OnPlayer2NameSubmitted);
-
-        // Add listeners for value changed events to handle Thai character input
-        p1InputField.onValueChanged.AddListener(OnPlayer1NameChanged);
-        p2InputField.onValueChanged.AddListener(OnPlayer2NameChanged);
     }
 
     // Called when Player 1 finishes typing their name
@@ -68,41 +61,5 @@ public class INameInput : MonoBehaviour
         INameData.SavePlayerName(2, playerName);
         p2DisplayText.text = playerName;
         Debug.Log("Player 2's name saved: " + playerName);
-    }
-
-    // Called when Player 1's name is changed
-    public void OnPlayer1NameChanged(string playerName)
-    {
-        p1InputField.text = LimitThaiCharacters(playerName, MaxThaiNameLength);
-    }
-
-    // Called when Player 2's name is changed
-    public void OnPlayer2NameChanged(string playerName)
-    {
-        p2InputField.text = LimitThaiCharacters(playerName, MaxThaiNameLength);
-    }
-
-    // Limit the number of Thai characters in the input
-    private string LimitThaiCharacters(string input, int maxLength)
-    {
-        int thaiCharCount = 0;
-        foreach (char c in input)
-        {
-            if (IsThaiCharacter(c))
-            {
-                thaiCharCount++;
-                if (thaiCharCount > maxLength)
-                {
-                    return input.Substring(0, input.Length - 1);
-                }
-            }
-        }
-        return input;
-    }
-
-    // Check if a character is a Thai character
-    private bool IsThaiCharacter(char c)
-    {
-        return c >= 0x0E00 && c <= 0x0E7F;
     }
 }
