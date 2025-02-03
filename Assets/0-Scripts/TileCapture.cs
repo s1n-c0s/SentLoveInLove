2a79ddcf325e1768a6904a3e3ff7c1f1eeddec50
TNK_Thanakrit <phumpuang_t@silpakorn.edu>
2025-02-03 13:58:02 +0700

    + Color Capture

--- Assets/0-Scripts/TileCapture.cs
+++ Assets/0-Scripts/TileCapture.cs
@@ -12,12 +12,25 @@ public class TileCapture : MonoBehaviour
     }
 
     [SerializeField] private Owner currentOwner = Owner.None;
+    [SerializeField] private Material personAMaterial;
+    [SerializeField] private Material personBMaterial;
+    [SerializeField] private Material defaultMaterial;
+    [SerializeField] private Renderer tileRenderer;
+
+    private void Start()
+    {
+        // tileRenderer = GetComponentInChildren<Renderer>();
+        if (tileRenderer != null)
+        {
+            tileRenderer.material = defaultMaterial;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent<PackageMover>(out var package))
         {
-            if (package.targetPerson.CompareTag("PersonB")) // -> Person A
+            if (package.targetPerson.CompareTag("PersonB"))
             {
                 if (currentOwner != Owner.PersonA)
                 {
@@ -27,6 +40,7 @@ public class TileCapture : MonoBehaviour
                     }
                     PlayerDataManager.Instance.IncrementTileA();
                     currentOwner = Owner.PersonA;
+                    UpdateTileMaterial();
                 }
             }
             else if (package.targetPerson.CompareTag("PersonA"))
@@ -39,6 +53,7 @@ public class TileCapture : MonoBehaviour
                     }
                     PlayerDataManager.Instance.IncrementTileB();
                     currentOwner = Owner.PersonB;
+                    UpdateTileMaterial();
                 }
             }
         }
@@ -52,6 +67,7 @@ public class TileCapture : MonoBehaviour
                 }
                 PlayerDataManager.Instance.IncrementTileA();
                 currentOwner = Owner.PersonA;
+                UpdateTileMaterial();
             }
         }
         else if (other.CompareTag("PersonB"))
@@ -64,6 +80,42 @@ public class TileCapture : MonoBehaviour
                 }
                 PlayerDataManager.Instance.IncrementTileB();
                 currentOwner = Owner.PersonB;
+                UpdateTileMaterial();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("PersonA") && currentOwner == Owner.PersonA)
+        {
+            PlayerDataManager.Instance.DecrementTileA();
+            currentOwner = Owner.None;
+            UpdateTileMaterial();
+        }
+        else if (other.CompareTag("PersonB") && currentOwner == Owner.PersonB)
+        {
+            PlayerDataManager.Instance.DecrementTileB();
+            currentOwner = Owner.None;
+            UpdateTileMaterial();
+        }
+    }
+
+    private void UpdateTileMaterial()
+    {
+        if (tileRenderer != null)
+        {
+            switch (currentOwner)
+            {
+                case Owner.PersonA:
+                    tileRenderer.material = personAMaterial;
+                    break;
+                case Owner.PersonB:
+                    tileRenderer.material = personBMaterial;
+                    break;
+                default:
+                    tileRenderer.material = defaultMaterial;
+                    break;
             }
         }
     }
