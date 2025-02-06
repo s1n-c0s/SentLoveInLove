bf7a5032546083009d1cf478972c5b1adf5cfd5a
TNK_Thanakrit <phumpuang_t@silpakorn.edu>
2025-02-06 11:58:57 +0700

    = rotate

---
+++ Assets/0-Scripts/LogoRotate.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class LogoRotate : MonoBehaviour
+{
+    public RectTransform imageTransform;
+    public float rotationSpeed = 20f;
+
+    private Quaternion targetRotation;
+
+    private void OnEnable()
+    {
+        targetRotation = imageTransform.localRotation;
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        targetRotation = Quaternion.Euler(0, 0, targetRotation.eulerAngles.z + rotationSpeed * Time.deltaTime);
+        imageTransform.localRotation = targetRotation;
+    }
+}
+
+
