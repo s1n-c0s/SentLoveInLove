c75455793c47d22b905e3190372c682943d8d352
TNK_Thanakrit <phumpuang_t@silpakorn.edu>
2025-02-06 14:24:18 +0700

    + size hover

--- Assets/0-Scripts/IHoverButton.cs
+++ Assets/0-Scripts/IHoverButton.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using PrimeTween;
 
-public class HoverButton : MonoBehaviour
+public class IHoverButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    private Vector3 originalScale;
+    private Vector3 hoverScale;
+    private RectTransform rectTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rectTransform = GetComponent<RectTransform>();
+        originalScale = rectTransform.localScale;
+        hoverScale = originalScale * 1.2f; // Scale up by 20%
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerEnter(PointerEventData eventData)
     {
-        
+        if (rectTransform.localScale != hoverScale)
+        {
+            Tween.Scale(rectTransform, hoverScale, 0.2f);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (rectTransform.localScale != originalScale)
+        {
+            Tween.Scale(rectTransform, originalScale, 0.2f);
+        }
     }
 }
