a20840517b62696ea4771b1fab54a8f5370c5d6b
TNK_Thanakrit <phumpuang_t@silpakorn.edu>
2025-02-11 13:03:37 +0700

    = optimize and refactor

--- Assets/0-Scripts/ISpamKey.cs
+++ Assets/0-Scripts/ISpamKey.cs
@@ -1,56 +1,53 @@
 using UnityEngine;
 using UnityEngine.UI;
 using PrimeTween;
-using System.Collections;
 
+[RequireComponent(typeof(Image), typeof(RectTransform))]
 public class ISpamKey : MonoBehaviour
 {
     [SerializeField] private bool isPersonA = true;
     [SerializeField] private Image spriteRenderer;
-    [SerializeField] private Sprite[] spriteSequence; // Animation frames
+    [SerializeField] private Sprite[] spriteSequence;
     [SerializeField] private float frameRate = 0.5f; // Time per frame
 
     private RectTransform rectTransform;
     private bool isShaking;
+    private float nextFrameTime;
+    private int frameIndex;
 
-    private void Start()
+    private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
+        spriteRenderer ??= GetComponent<Image>(); // Assign if not set in Inspector
+    }
 
-        if (spriteRenderer == null)
+    private void OnEnable()
+    {
+        frameIndex = 0;
+        nextFrameTime = Time.time + frameRate;
+        spriteRenderer.sprite = spriteSequence[frameIndex]; // Ensure correct frame on reactivation
+    }
+
+    private void Update()
+    {
+        if (Time.time >= nextFrameTime)
         {
-            spriteRenderer = GetComponent<Image>();
+            nextFrameTime += frameRate;
+            frameIndex = (frameIndex + 1) % spriteSequence.Length;
+            spriteRenderer.sprite = spriteSequence[frameIndex];
         }
-
-        // Start the looping animation
-        StartCoroutine(PlaySpriteSequenceLoop());
     }
 
     public void OnKeyPress()
     {
-        if (!isShaking)
+        if (isShaking) return;
+
+        isShaking = true;
+        Tween.ShakeScale(rectTransform, new Vector3(0.5f, 0.5f, 0f), 0.1f).OnComplete(() =>
         {
-            isShaking = true;
-            Tween.ShakeScale(rectTransform, new Vector3(0.5f, 0.5f, 0f), 0.1f).OnComplete(() =>
-            {
-                isShaking = false;
-            });
-        }
+            isShaking = false;
+        });
     }
 
-    private IEnumerator PlaySpriteSequenceLoop()
-    {
-        int index = 0;
-        while (true) // Infinite loop
-        {
-            spriteRenderer.sprite = spriteSequence[index];
-            index = (index + 1) % spriteSequence.Length; // Loop back to the start
-            yield return new WaitForSeconds(frameRate);
-        }
-    }
-
-    public bool IsPersonA()
-    {
-        return isPersonA;
-    }
+    public bool IsPersonA() => isPersonA;
 }
