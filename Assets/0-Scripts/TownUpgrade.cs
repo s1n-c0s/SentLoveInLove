ca443b4e184745c698f271e56ee05b946d291ae5
TNK_Thanakrit <phumpuang_t@silpakorn.edu>
2025-06-24 14:04:17 +0700

    + Upgrade Town

---
+++ Assets/0-Scripts/TownUpgrade.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class TownUpgrade : MonoBehaviour
+{
+    [SerializeField] private GameObject[] upgradeVisuals; // Array of upgrade visuals
+    public bool canUpgrade = false; // Flag to check if the town can be upgraded
+    public int currentUpgradeLevel = 0; // Current upgrade level
+
+
+    public void UpgradeTown()
+    {
+        upgradeVisuals[currentUpgradeLevel].SetActive(false);
+        currentUpgradeLevel++;
+        upgradeVisuals[currentUpgradeLevel].SetActive(true);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Package") && canUpgrade)
+        {
+            if (currentUpgradeLevel > 3)
+            {
+                canUpgrade = false;
+            }
+            else
+            {
+                UpgradeTown();
+            }
+        }
+    }
+}
