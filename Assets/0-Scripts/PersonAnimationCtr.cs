648c2cadbf2818b78b91144cb67c8964118efc09
TNK_Thanakrit <phumpuang_t@silpakorn.edu>
2025-08-17 21:17:21 +0700

    + Play Animation Stage

---
+++ Assets/0-Scripts/PersonAnimationCtr.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonAnimationCtr : MonoBehaviour
+{
+    [Header("Animations")]
+    private Animator _animator;
+    private string _CurrentAnimation;
+
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void SetAnimation(string animationName)
+    {
+        float transitionDuration = 0.2f;
+        if (_animator == null)
+        {
+            Debug.LogError("Animator is not assigned or found in the children.");
+            return;
+        }
+
+        if (_CurrentAnimation != animationName)
+        {
+            _CurrentAnimation = animationName;
+            _animator.CrossFade(animationName, transitionDuration);
+        }
+    }
+}
